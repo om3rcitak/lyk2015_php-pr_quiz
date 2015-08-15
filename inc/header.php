4794d34edb286bc1ac5f9020789d00bc10b25e37
Ömer İPEK <wb_omer@hotmail.com>
2015-08-15 16:06:51 +0300

    Responsive Meta
    I added responsive meta tag.

--- inc/header.php
+++ inc/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LYK 2015 Php - Pr Quiz</title>
     <link rel="stylesheet" href="assets/bootstrap.min.css">
     <style>
