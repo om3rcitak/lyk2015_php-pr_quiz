8098a4ad13638ac2ee4aec46fc82d1c2c7943348
Uğur Arıcı <ugurarici@users.noreply.github.com>
2015-08-17 17:11:32 +0300

    Update queston.php for db connection

--- inc/questions.php
+++ inc/questions.php
@@ -1,21 +1,58 @@
 <?php
+
 function getAllQuestions($path = ""){
+    $questions = array();
     $doWeHaveData = false;
-    $questionsDataFilePath = $path. "data/questions.txt";
+    // mysql sunucusuna bağlan
+    $connection = mysql_connect("localhost", "root", "13066529015");
+    if( ! $connection)
+        die("MySQL sunucuna bağlanamadı");
+
+// bağlantı üstünden utf8 standartında konuşacağımızı söylüyoruz
+    mysql_set_charset("UTF8", $connection);
+
+// çalışacağımız veritabanını seç
+    if( ! mysql_select_db("quiz", $connection))
+        die("Veritabanı seçilemedi");
+
+// belirli bir tablodaki tüm verileri çek
+    $questionsRS = mysql_query("SELECT * FROM  questions");
 
 
-    if(file_exists($questionsDataFilePath)) {
-        $questionsDataFile = fopen($questionsDataFilePath, "r");
-        $encodedQuestions = fread($questionsDataFile, filesize($questionsDataFilePath));
-        fclose($questionsDataFile);
-
-        if($encodedQuestions){
-            //dizi yapıp ver
-            $questions = json_decode($encodedQuestions, true);
-            $doWeHaveData = true;
-        }
+// diziye çevir, kullan
+    while($question = mysql_fetch_array($questionsRS, MYSQL_ASSOC)){
+        $questions[] = array(
+            'question' => $question['question'],
+            'options' => array(
+                'a' => $question['option_a'],
+                'b' => $question['option_b'],
+                'c' => $question['option_c'],
+                'd' => $question['option_d'],
+            ),
+            'answer' => $question['answer']
+        );
     }
 
+    if(count($questions)>0) $doWeHaveData = true;
+
+// diziyi istenilen çıktı haline getir
+
+    if(! $doWeHaveData) {
+        $questionsDataFilePath = $path . "data/questions.txt";
+
+
+        if (file_exists($questionsDataFilePath)) {
+            $questionsDataFile = fopen($questionsDataFilePath, "r");
+            $encodedQuestions = fread($questionsDataFile, filesize($questionsDataFilePath));
+            fclose($questionsDataFile);
+
+            if ($encodedQuestions) {
+                //dizi yapıp ver
+                $questions = json_decode($encodedQuestions, true);
+                if(count($questions)>0) $doWeHaveData = true;
+            }
+        }
+    }
 
     // yoksa standart diziyi gönder
     if(!$doWeHaveData) {
@@ -31,6 +68,8 @@ function getAllQuestions($path = ""){
                 'answer' => 'd'
             ),
         );
+        $doWeHaveData = true;
     }
+
     return $questions;
 }
